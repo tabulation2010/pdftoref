49d63c90574be9baf7748c4754177a13075d8b09
iacopo.masi <iacopo.masi@67bede5f-2a4f-0410-ad5b-e3d54fc54580>
2008-06-18 14:15:14 +0000

    switch to the last version of pdfminer (r29)

--- trunk/src/Pdfminer/pdfinterp.py
+++ trunk/src/Pdfminer/pdfinterp.py
@@ -957,18 +957,15 @@ class PDFPageInterpreter:
       print >>stderr, 'Processing xobj: %r' % xobj
     subtype = xobj.dic.get('Subtype')
     if subtype == LITERAL_FORM and 'BBox' in xobj.dic:
-      try:
-          interpreter = PDFPageInterpreter(self.rsrc, self.device)
-          (x0,y0,x1,y1) = xobj.dic['BBox']
-          ctm = mult_matrix(xobj.dic.get('Matrix', MATRIX_IDENTITY), self.ctm)
-          (x0,y0) = apply_matrix(ctm, (x0,y0))
-          (x1,y1) = apply_matrix(ctm, (x1,y1))
-          bbox = (x0,y0,x1,y1)
-          self.device.begin_figure(xobjid, bbox)
-          interpreter.render_contents(xobj.dic.get('Resources'), [xobj], ctm=ctm)
-          self.device.end_figure(xobjid)
-      except TypeError:
-          pass
+      interpreter = PDFPageInterpreter(self.rsrc, self.device)
+      (x0,y0,x1,y1) = xobj.dic['BBox']
+      ctm = mult_matrix(xobj.dic.get('Matrix', MATRIX_IDENTITY), self.ctm)
+      (x0,y0) = apply_matrix(ctm, (x0,y0))
+      (x1,y1) = apply_matrix(ctm, (x1,y1))
+      bbox = (x0,y0,x1,y1)
+      self.device.begin_figure(xobjid, bbox)
+      interpreter.render_contents(xobj.dic.get('Resources'), [xobj], ctm=ctm)
+      self.device.end_figure(xobjid)
     elif subtype == LITERAL_IMAGE and 'Width' in xobj.dic and 'Height' in xobj.dic:
       (x0,y0) = apply_matrix(self.ctm, (0,0))
       (x1,y1) = apply_matrix(self.ctm, (1,1))
